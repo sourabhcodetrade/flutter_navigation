50d67243ac43ceb0320067e78b7cda35125ad549
sourabh <sourabh.sharma@mail.codetrade.io>
2024-12-17 18:49:32 +0530

    [Add] color constants

---
+++ lib/utils/constants/color_constants.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+final class ColorConstants {
+  static const Color primaryOldColor = Color(0xff0D73CF),
+      primaryColor = Color(0xff008BD0),
+      secondaryColor = Color(0xfff4f2f8),
+      primaryBrightColor = Color(0xff008BD0),
+      whiteColor = Colors.white,
+      blackColor = Colors.black,
+      redColor = Colors.red,
+      greyColor = Color(0xff9E9E9E),
+      lightGreyColor = Color(0xffDEDEDE),
+      lightTextGreyColor = Color(0xff5A5C5E),
+      shadowColor = Color(0xffD6D6D6),
+      shadowColorFilter = Color(0xffECECEC),
+      bottomNavBarUnselectedColor = Color(0xffCACACA),
+      dividerColor = Color(0xff707070),
+      unselectedLabelColor = Color(0xff777777),
+      searchFieldTextColor = Color(0xFF848484),
+      greenColor = Color(0xff2AB889),
+      greenLightColor = Color(0xff85CDB4),
+      greenLightColor2 = Color(0xff18B28F),
+      greenColor2 = Color(0xff22b44e),
+      lightGreyTextColor = Color(0xFFA4A4A4),
+      textFieldIconColor = Color(0xFFA5A5A5),
+      casesDivider = Color(0xFFF2EBFF),
+      // scheduleDivider = Color(0xFFC6C6C6),
+      casesStatusBgColor = Color(0xffF5F5F5),
+      distolicColor = Color(0xff30B888),
+      heartRateColor = Color(0xffEB2C37),
+      redGenderIconColor = Color(0xffFF5D9E),
+      redGenderIconColor22 = Color(0xffc580e0),
+      chatBubbleColor = Color(0xff9377c5),
+      // DashboardTileColors
+      selectedAssessmentBgColor = Color(0xfffaf9fd),
+      // DashboardTileColors
+      colorOrange = Color(0xffe54f1a),
+      colorGreen = Color(0xff30b888),
+      appointmentTileColor = Color(0xffe7a037),
+      myAppointmentTileColor = Color(0xff2d6fb7),
+      schedulesTileColor = Color(0xffee2d38),
+      scaffoldBgColor = Color(0xFFe9eef4),
+      lightPrimaryColor = Color.fromRGBO(13, 115, 207, 0.07),
+      inputErrorTextColor = Color(0xffb00020),
+      lightPrimaryBg = Color(0xffdfeaf1);
+  static Color timelineBorderColor =
+      ColorConstants.primaryColor.withValues(alpha: 0.5);
+}
